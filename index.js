6e625dfc90530b482f530674ffec49abb65f63b0
mercy <macivy49@gmail.com>
2022-09-09 13:42:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Write your algorithm here 
+  
+for (let i = 0; i < word.length / 2; i++) {
+  // compare the letter we're iterating over to the corresponding letter at the end of the string
+  const j = word.length - 1 - i;
+  if (word[i] !== word[j]) {
+    // if the letters don't match, return false
+    return false;
+  }
 }
 
+// if we reach the middle, and all the letters match, return true
+return true;
+}
 /* 
   Add your pseudocode here
 */
@@ -18,8 +29,28 @@ if (require.main === module) {
 
   console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>",isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
 }
 
 module.exports = isPalindrome;
